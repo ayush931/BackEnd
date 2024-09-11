aef386f04384b647ef60e4c845daab3d7964801a
ayush931 <ayushkumar9315983@gmail.com>
2024-09-11 15:59:59 +0530

    Mongoose Schema

---
+++ 02mongoose/models/todos/user.model.js
@@ -0,0 +1,24 @@
+import { Timestamp } from 'bson'
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true 
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+
+  }
+}, {timestamps: true})
+
+export default User = mongoose.model("User", userSchema)
